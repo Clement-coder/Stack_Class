85b92ad43fa1dda32a28a8fb7069319c2f07fc5f
Clement Raymond <chinexzy37@gmail.com>
2025-07-02 14:19:38 +0000

    initial commit

--- script.js
+++ script.js
@@ -18,11 +18,50 @@ class Stack {
         return item
     }
 
+
+    print(){
+        let output = "";
+        for(let i = this.length-1; i >= 0; i--){
+            let seperator = ''
+            if(i > 0 ) {
+                    seperator = ' -> '
+            }
+            output += `${this.item[i]}${seperator}`
+        }
+        return output;
+    }
+
+    peek() {
+        if(this.length == 0){
+            return null
+        }
+            return this.item[this.length-1]
+
+    }
+
+    size() {
+        return this .length;
+    }
+      isEmpty() {
+        return this .length == 0;
+    }
+
 }
 const stack = new Stack();
 stack.push(10)
 stack.push(500)
+stack.push(20)
+stack.push(40)
 
-console.log(stack.length)
-console.log(stack.pop())
-console.log(stack.length)
+console.log(`* ${stack.isEmpty()}`)
+console.log(`* ${stack.length}`)
+console.log(`* ${stack.pop()}`)
+console.log(`* ${stack.pop()}`)
+console.log(`* ${stack.pop()}`)
+console.log(`* ${stack.pop()}`)
+console.log(`* ${stack.length}`)
+console.log(`* ${stack.print()}`)
+console.log(`* ${stack.peek()}`)
+console.log(`* ${stack.item}`)
+console.log(`* ${stack.size()}`)
+console.log(`* ${stack.isEmpty()}`)
